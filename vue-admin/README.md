a4f6dbe53e40405d48278db38bde82635b312616
Hailie17 <192151452@qq.com>
2024-04-12 22:54:06 +0800

    delete .git

---
+++ vue-admin/README.md
@@ -0,0 +1,16 @@
+# vue-admin
+
+> 基于 vue-admin-template 的二次定制脚手架，去掉了一些繁琐的东西，升级了一些包
+
+**Live demo:** http://panjiachen.github.io/vue-admin-template
+
+后台管理在线 MD 文档
+
+https://www.yuque.com/fechaichai/ek8r5d
+
+接口
+https://apifox.com/apidoc/shared-056d7c04-303c-43cb-9b42-8e3d457844f5
+
+前台可视化在线 MD 文档
+
+https://www.yuque.com/fechaichai/biqso7
---
+++ vue-admin/src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+// 类似 reset.css 清空默认样式
+import 'normalize.css/normalize.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+
+import store from './store'
+
+import router from './router'
+// 注册svg
+import '@/icons'
+
+Vue.use(ElementUI)
+
+// 上线后，控制台不显示警告
+Vue.config.productionTip = false
+
+// vue实例化
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
