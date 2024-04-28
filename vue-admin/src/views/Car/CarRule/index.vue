7904806e8cd17f269b8a8f630500014c9aa8cb78
Hailie17 <192151452@qq.com>
2024-04-28 23:48:22 +0800

    分页bug修改

--- vue-admin/src/views/Car/CarRule/index.vue
+++ vue-admin/src/views/Car/CarRule/index.vue
@@ -26,7 +26,7 @@
         </el-table-column>
       </el-table>
       <div class="page-container">
-        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="params.current" :page-sizes="[10, 20, 30, 40]" :page-size="params.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total"> </el-pagination>
+        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="params.page" :page-sizes="[2, 4, 10, 40]" :page-size="params.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total"> </el-pagination>
       </div>
     </div>
   </div>
