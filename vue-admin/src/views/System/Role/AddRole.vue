ae5d62629c431e6ebb33711b4a76af99c3e9f434
Hailie17 <192151452@qq.com>
2024-05-03 18:55:13 +0800

    新增角色页面

--- vue-admin/src/views/System/Role/AddRole.vue
+++ vue-admin/src/views/System/Role/AddRole.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="add-role">
+    <header class="add-header">
+      <div class="left">
+        <span class="arrow" @click="$router.back()"><i class="el-icon-arrow-left" />返回</span>
+        <span>|</span>
+        <span>添加角色</span>
+      </div>
+      <div class="right">这是第n天</div>
+    </header>
     <main class="add-main">
       <div class="step-container">
         <el-steps direction="vertical" :active="activeStep">
@@ -49,3 +57,107 @@ export default {
   }
 }
 </script>
+<style scoped lang="scss">
+.add-role {
+  background-color: #f4f6f8;
+  height: 100vh;
+
+  .add-header {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 20px;
+    height: 64px;
+    line-height: 64px;
+    font-size: 16px;
+    font-weight: bold;
+    background-color: #fff;
+
+    .left {
+      span {
+        margin-right: 4px;
+      }
+
+      .arrow {
+        cursor: pointer;
+      }
+    }
+
+    .right {
+      text-align: right;
+    }
+  }
+
+  .add-main {
+    position: fixed;
+    top: 64px;
+    bottom: 88px;
+    width: 100%;
+    overflow-y: auto;
+    background: #f4f6f8;
+    padding: 20px 230px;
+    display: flex;
+    .step-container {
+      height: 400px;
+      width: 200px;
+    }
+    .form-container {
+      flex: 1;
+      background-color: #fff;
+      overflow-y: auto;
+      .title {
+        height: 60px;
+        line-height: 60px;
+        padding-left: 20px;
+      }
+      .form {
+        margin-bottom: 20px;
+        padding: 20px 65px 24px;
+        .el-form {
+          display: flex;
+          flex-wrap: wrap;
+        }
+        .info {
+          font-size: 14px;
+          color: #666;
+          .form-item {
+            margin-bottom: 20px;
+          }
+        }
+      }
+      .form-box {
+        width: 600px;
+        display: flex;
+        flex-direction: column;
+      }
+    }
+  }
+
+  .add-footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    padding: 24px 50px;
+    color: #000000d9;
+    font-size: 14px;
+    background: #fff;
+    text-align: center;
+    z-index: 10001;
+  }
+  .tree-wrapper {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    .tree-item {
+      flex: 1;
+      padding: 0px 4px;
+      text-align: center;
+      .tree-title {
+        background: #f5f7fa;
+        text-align: center;
+        padding: 10px 0;
+        margin-bottom: 12px;
+      }
+    }
+  }
+}
+</style>
