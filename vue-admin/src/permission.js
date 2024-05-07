3bebe1b909aed58df61f01f0c26e52d1f45f3b01
Hailie17 <192151452@qq.com>
2024-05-07 18:43:35 +0800

    // 获取二级权限标识

--- vue-admin/src/permission.js
+++ vue-admin/src/permission.js
@@ -6,8 +6,16 @@ function getFirstPermission(permissions) {
   const firtsPermArr = permissions.map(item => {
     return item.split(':')[0]
   })
-  // 去重 set不能存在重复的数组
+  // 去重 set不能存在重复的数组 但她不是一个真的数组
   return Array.from(new Set(firtsPermArr))
+}
+// 获取二级权限标识
+function getSecondPermission(permissions) {
+  const secondPermArr = permissions.map(item => {
+    const arr = item.split(':')
+    return `${arr[0]}:${arr[1]}`
+  })
+  return Array.from(new Set(secondPermArr))
 }
 
 router.beforeEach(async (to, from, next) => {
@@ -19,8 +27,8 @@ router.beforeEach(async (to, from, next) => {
       next()
       await store.dispatch('menu/getProfile')
       const profile = store.state.menu.profile
-      console.log(profile)
-      getFirstPermission(profile.permissions)
+      const firstPermissions = getFirstPermission(profile.permissions)
+      const secondPermissions = getSecondPermission(profile.permissions)
     }
   } else {
     if (whiteList.includes(to.path)) {
