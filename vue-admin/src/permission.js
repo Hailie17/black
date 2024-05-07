fa8408ce832f6c83628e40049ad71fe247d59e6e
Hailie17 <192151452@qq.com>
2024-05-07 18:34:41 +0800

    获取一级权限标识

--- vue-admin/src/permission.js
+++ vue-admin/src/permission.js
@@ -1,15 +1,26 @@
 import router from '@/router'
 import store from '@/store'
 const whiteList = ['/login', '/404']
+// 获取一级权限标识
+function getFirstPermission(permissions) {
+  const firtsPermArr = permissions.map(item => {
+    return item.split(':')[0]
+  })
+  // 去重 set不能存在重复的数组
+  return Array.from(new Set(firtsPermArr))
+}
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const token = store.state.user.token
   if (token) {
     if (to.path === '/login') {
       next('/')
     } else {
       next()
-      store.dispatch('menu/getProfile')
+      await store.dispatch('menu/getProfile')
+      const profile = store.state.menu.profile
+      console.log(profile)
+      getFirstPermission(profile.permissions)
     }
   } else {
     if (whiteList.includes(to.path)) {
