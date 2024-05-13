50194a834bfb1e6d6403ac8ae42bc47ce002d4c7
Hailie17 <192151452@qq.com>
2024-05-13 18:06:24 +0800

    自定义指令

--- vue-admin/src/directive/index.js
+++ vue-admin/src/directive/index.js
@@ -3,5 +3,9 @@ import Vue from 'vue'
 import store from '@/store'
 
 Vue.directive('permission', {
-  inserted(el, binding) {}
+  // el 自定义指令的dom元素
+  // binding 自定义指令的绑定值
+  inserted(el, binding) {
+    const perms = store.state.menu.profile.permission
+  }
 })
