d027448d49a492f8c76bdd768e52461c8c6245b9
Hailie17 <192151452@qq.com>
2024-05-17 15:04:07 +0800

    搭建路由

--- vue3-project/src/router/index.js
+++ vue3-project/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BigScreenView from '../views/BigScreenView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,8 +9,13 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/big-screen',
+      name: 'big-screen',
+      component: BigScreenView
     }
-  ]
+    ]
 })
 
 
