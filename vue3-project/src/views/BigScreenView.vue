779128ea9232ad40eb9d15d34c4b249ae6353b2e
Hailie17 <192151452@qq.com>
2024-05-19 16:52:20 +0800

    抽离组合式函数

--- vue3-project/src/views/BigScreenView.vue
+++ vue3-project/src/views/BigScreenView.vue
@@ -1,57 +1,13 @@
 <script setup>
-import {ref, onMounted} from 'vue'
-import * as echarts from 'echarts'
+import { onMounted} from 'vue'
 
-import {useBigScreen, useInitBarChart} from '@/views/composables/bigscreen'
+import {useBigScreen, useInitBarChart, useInitPieChart} from '@/views/composables/bigscreen'
 
 const {parkInfo, getParkInfo} = useBigScreen()
+
 const {barChart, initBarChart} = useInitBarChart(parkInfo)
 
-
-const pieChart = ref(null)
-
-const initPieChart = () => {
-  const { parkIndustry } = parkInfo.value
-  const pieOption = {
-    color: [
-      '#00B2FF', '#2CF2FF', '#892CFF', '#FF624D', '#FFCF54', '#86ECA2'],
-    legend: {
-      itemGap: 20,
-      bottom: '0',
-      icon: 'rect',
-      itemHeight: 10, // 图例icon高度
-      itemWidth: 10, // 图例icon宽度
-      textStyle: {
-        color: '#c6d1db',
-      },
-    },
-    tooltip: {
-      trigger: 'item'
-    },
-    series: [
-      {
-        name: '园区产业分析',
-        type: 'pie',
-        radius: ['55%', '60%'], // 设置内圈与外圈的半径使其呈现为环形
-        center: ['50%', '40%'], // 圆心位置， 用于调整整个图的位置
-        tooltip: {
-          trigger: 'item',
-          formatter: (params) => {
-            return `${params.seriesName}</br><div style='display:flex;justify-content: space-between;'><div>${params.marker}${params.name}</div><div>${params.percent}%</div></div>`
-          }
-        },
-        label: {
-          show: false,
-          position: 'center',
-        },
-        data: parkIndustry,
-      },
-    ],
-
-  }
-  const myPieChart = echarts.init(pieChart.value)
-  myPieChart.setOption(pieOption)
-}
+const {pieChart, initPieChart} = useInitPieChart(parkInfo)
 
 onMounted(async () => {
   await getParkInfo()
