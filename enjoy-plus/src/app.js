73e35040064b5b95a3131b2a641536b1daaefffa
Hailie17 <192151452@qq.com>
2024-05-30 13:48:18 +0800

    获取验证码

--- enjoy-plus/src/app.js
+++ enjoy-plus/src/app.js
@@ -6,6 +6,6 @@ wx.utils = utils
 wx.http = http
 
 App({
-  token: wx.batchGetStorageSync(['token']),
+  token: wx.getStorageSync('token'),
   globalData: {},
 })
--- enjoy-plus/src/pages/login/index.js
+++ enjoy-plus/src/pages/login/index.js
@@ -1,6 +1,8 @@
 Page({
   data: {
-    countDownVisible: true,
+    countDownVisible: false,
+    mobile:'',
+    code: ''
   },
 // 倒计时显示时间
   countDownChange(ev) {
@@ -9,4 +11,13 @@ Page({
       countDownVisible: ev.detail.minutes === 1 || ev.detail.seconds > 0,
     })
   },
+  async getSMSCode(){
+    this.setData({
+      countDownVisible: true
+    })
+    const res = await wx.http.get('/code', {mobile: this.data.mobile})
+    this.setData({
+      code:res.data.code
+    })
+  }
 })
