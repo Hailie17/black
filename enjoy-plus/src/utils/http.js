11ecb0696c78b6356dfbd4008bef83e85477e0ad
Hailie17 <192151452@qq.com>
2024-06-04 17:51:38 +0800

    设置请求拦截器 添加token

--- enjoy-plus/src/utils/http.js
+++ enjoy-plus/src/utils/http.js
@@ -1,6 +1,15 @@
 import http from 'wechat-http'
 http.baseURL = 'https://live-api.itheima.net'
 
+//请求拦截器
+http.intercept.request = (config) => {
+  config.header = {
+    Authorization: 'Bearer ' + wx.getStorageSync('token'),
+    ...config.header
+  }
+  return config
+}
+
 // 响应拦截器
 http.intercept.response = (res) => {
   if (res.data.code == 10000) {
