6a17081932590f3ff9760c4171d71e2ce63a5f86
Hailie17 <192151452@qq.com>
2024-06-06 18:42:28 +0800

    无感刷新token

--- enjoy-plus/src/app.js
+++ enjoy-plus/src/app.js
@@ -6,7 +6,15 @@ wx.utils = utils
 wx.http = http
 
 App({
-  token: wx.getStorageSync('token') || '',
+  onLaunch() {
+    // 读取 token
+    this.getToken()
+  },
+  getToken() {
+    // 读取本地的token
+    this.token = wx.getStorageSync('token')
+    this.refreshToken = wx.getStorageSync('refreshToken')
+  },
   setToken(key,token){
     this[key] = token
     wx.setStorageSync(key, token)
