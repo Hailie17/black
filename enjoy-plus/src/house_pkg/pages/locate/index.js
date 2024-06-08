988978a9ef1d5a8fab6208dea8ee1bf0639e259b
Hailie17 <192151452@qq.com>
2024-06-08 16:44:50 +0800

    腾讯位置服务，小区列表渲染

--- enjoy-plus/src/house_pkg/pages/locate/index.js
+++ enjoy-plus/src/house_pkg/pages/locate/index.js
@@ -4,6 +4,9 @@ const qqMapSDK = new QQMapWX({
   key: 'PLUBZ-JE2RJ-T2FFK-XLW7V-W7X4S-QVFV5',
 })
 Page({
+  data: {
+    list: []
+  },
   onLoad() {
     this.search()
   },
@@ -11,8 +14,12 @@ Page({
     // 调用腾讯位置服务的搜索
     qqMapSDK.search({
       keyword: '住在小区',
+      page_size: 5,
       success: (res) => {
         console.log('res', res)
+        this.setData({
+          list: res.data
+        })
       },
     })
   },
