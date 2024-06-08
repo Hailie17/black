0d68d44ab4c668508cf4c9f9eb6120bebc0db008
Hailie17 <192151452@qq.com>
2024-06-08 16:44:19 +0800

    调用腾讯位置服务的搜索

--- enjoy-plus/src/house_pkg/pages/locate/index.js
+++ enjoy-plus/src/house_pkg/pages/locate/index.js
@@ -1,8 +1,19 @@
 // house_pkg/pages/locate/index.ts
 import QQMapWX from '../../../libs/qqmap-wx-jssdk'
 const qqMapSDK = new QQMapWX({
-  key: 'PLUBZ-JE2RJ-T2FFK-XLW7V-W7X4S-QVFV5'
+  key: 'PLUBZ-JE2RJ-T2FFK-XLW7V-W7X4S-QVFV5',
 })
 Page({
-
+  onLoad() {
+    this.search()
+  },
+  search() {
+    // 调用腾讯位置服务的搜索
+    qqMapSDK.search({
+      keyword: '住在小区',
+      success: (res) => {
+        console.log('res', res)
+      },
+    })
+  },
 })
