e44aa7f46309fa166f07f12cc87417fc4d7c6a6e
Hailie17 <192151452@qq.com>
2024-06-26 21:38:47 +0800

    房屋列表渲染

--- enjoy-plus/src/house_pkg/pages/list/index.js
+++ enjoy-plus/src/house_pkg/pages/list/index.js
@@ -1,17 +1,24 @@
 Page({
   data: {
+    list: [],
     dialogVisible: false,
   },
-
+  onShow(){
+    this.getList()
+  },
+  async getList(){
+    const res = await wx.http.get('/room')
+    this.setData({
+      list: res.data
+    })
+  },
   swipeClose(ev) {
     const { position, instance } = ev.detail
-
     if (position === 'right') {
       // 显示 Dialog 对话框
       this.setData({
         dialogVisible: true,
       })
-
       // swiper-cell 滑块关闭
       instance.close()
     }
