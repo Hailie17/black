02e285f92f0b39c2404083c82101717722eb3ba4
Hailie17 <192151452@qq.com>
2024-07-02 23:45:39 +0800

    地图组件

--- enjoy-plus/src/repair_pkg/pages/detail/index.js
+++ enjoy-plus/src/repair_pkg/pages/detail/index.js
@@ -3,5 +3,23 @@ Page({
   data: {
     latitude: 40.060539,
     longitude: 116.343847,
+    scale: 16,
+    markers: [
+      {
+        id: 1,
+        latitude: 40.060539,
+        longitude: 116.343847,
+        width: 32,
+        height: 50
+      },
+      {
+        id: 2,
+        latitude: 40.061539,
+        longitude: 116.341847,
+        iconPath: '../../../static/images/marker.png',
+        width: 32,
+        height: 50
+      }
+    ]
   },
 })
