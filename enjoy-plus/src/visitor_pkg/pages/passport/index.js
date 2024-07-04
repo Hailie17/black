47425c59024598612007e77f289c6cefea9a6ebc
Hailie17 <192151452@qq.com>
2024-07-04 23:09:23 +0800

    授权api，获取保存图片的授权

--- enjoy-plus/src/visitor_pkg/pages/passport/index.js
+++ enjoy-plus/src/visitor_pkg/pages/passport/index.js
@@ -6,6 +6,29 @@ Page({
       imageUrl: 'https://enjoy-plus.oss-cn-beijing.aliyuncs.com/images/share_poster.png',
     }
   },
+  authSetting(){
+    wx.getSetting({
+      success: ({authSetting}) => {
+        if (authSetting['scope.writePhotosAlbum'] === false) {
+          wx.showModal({
+            content: '请允许添加到相册',
+            showCancel: false,
+            success: () => {
+              wx.openSetting({
+                success: ({authSetting})=> {
+                  if (authSetting['scope.writePhotosAlbum']) {
+                    this.saveImage()
+                  }
+                }
+              })
+            }
+          })
+        }else {
+          this.saveImage()
+        }
+      }
+    })
+  },
   async saveImage(){
     const {path} = await wx.getImageInfo({
       src: 'https://enjoy-plus.oss-cn-beijing.aliyuncs.com/uploads/qrcode.png',
