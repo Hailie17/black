c8f8e75ee131b104f6941fd4a9d2f1fd5a3fd3c0
Hailie17 <192151452@qq.com>
2024-07-08 00:05:32 +0800

    完善token刷新

--- enjoy-plus/src/utils/http.js
+++ enjoy-plus/src/utils/http.js
@@ -15,11 +15,27 @@ http.intercept.response = async (res) => {
   if (res.data.code == 10000) {
     return res.data
   }else if(res.data.code == 401){
+    const app = getApp()
+    console.log(res.config.url,766);
+    if (res.config.url.includes('/refreshToken')) {
+      app.setToken('token', '')
+      app.setToken('refreshToken', '')
+      const pages= getCurrentPages() // 获取当前页
+      const page = pages[pages.length - 1]
+      wx.redirectTo({
+        url: 'page/login/index?redirectUrl=/' + page.route,
+      })
+      return
+    }
     const res2 = await http.post('/refreshToken',{},{header: {Authorization: 'Bearer ' + wx.getStorageSync('refreshToken')}})
     console.log(res2,222);
-    const app = getApp()
     app.setToken('token', res2.data.token)
     app.setToken('refreshToken', res2.data.refreshToken)
+    res.config.header = {
+      Authorization: 'Bearer ' + wx.getStorageSync('token'),
+    }
+    // 重新获取请求
+    return http(res.config)
   } else {
     wx.utils.toast(res.data.message || '业务错误')
     return Promise.reject(res.data)
