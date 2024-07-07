ba9e1b47e13cdb04414645899d7609b6474018b2
Hailie17 <192151452@qq.com>
2024-07-07 23:40:20 +0800

    过期token续期，无感刷新

--- enjoy-plus/src/utils/http.js
+++ enjoy-plus/src/utils/http.js
@@ -11,9 +11,15 @@ http.intercept.request = (config) => {
 }
 
 // 响应拦截器
-http.intercept.response = (res) => {
+http.intercept.response = async (res) => {
   if (res.data.code == 10000) {
     return res.data
+  }else if(res.data.code == 401){
+    const res2 = await http.post('/refreshToken',{},{header: {Authorization: 'Bearer ' + wx.getStorageSync('refreshToken')}})
+    console.log(res2,222);
+    const app = getApp()
+    app.setToken('token', res2.data.token)
+    app.setToken('refreshToken', res2.data.refreshToken)
   } else {
     wx.utils.toast(res.data.message || '业务错误')
     return Promise.reject(res.data)
